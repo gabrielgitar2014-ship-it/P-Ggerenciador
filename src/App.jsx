31c89e219a2ae93543227089e2b455997f07bd69
gabrielgitar2014-ship-it <gabriel.gitar2014@gmail.com>
2025-09-18 17:35:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { ThemeProvider } from './context/ThemeContext';
 import { FinanceProvider } from './context/FinanceContext';
 import { ModalProvider } from './context/ModalContext';
+import { VisibilityProvider } from './context/VisibilityContext'; // Importar
 import Dashboard from './pages/Dashboard';
 import PwaUpdater from './components/PwaUpdater';
 
 function App() {
   return (
     <ThemeProvider>
-      {/* 👇 A ORDEM FOI CORRIGIDA AQUI 👇 */}
       <FinanceProvider>
         <ModalProvider>
-          <PwaUpdater />
-          <Dashboard />
+          {/* Envolver a aplicação com o novo provedor */}
+          <VisibilityProvider>
+            <PwaUpdater />
+            <Dashboard />
+          </VisibilityProvider>
         </ModalProvider>
       </FinanceProvider>
     </ThemeProvider>
